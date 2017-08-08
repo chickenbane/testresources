8e5f816181fc7cf23e11d3e40c8ca99825770fea
joey t <thechickenbane@gmail.com>
2017-08-08 15:03:01 -0700

    Add a resource file, attempt to read it
    all three tests will pass with ./gradlew test
    
    testFile() fails in IDE

--- app/src/test/java/com/googlejobapp/testresources/ExampleUnitTest.kt
+++ app/src/test/java/com/googlejobapp/testresources/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.googlejobapp.testresources
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
+import java.io.File
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +15,32 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun testFile() {
+        val file = File("src/test/resources/hardToRead.json")
+        assertTrue(file.exists())
+        val string = file.readText()
+        assertEquals("content length", 38, string.length)
+    }
+
+    @Test
+    fun testClassLoaderResource() {
+        val resource = javaClass.classLoader.getResource("hardToRead.json")
+        assertNotNull(resource)
+        val file = File(resource.file)
+        assertTrue(file.exists())
+        val string = file.readText()
+        System.out.println("my string=$string")
+        assertEquals("content length", 38, string.length)
+    }
+
+    @Test
+    fun testClassLoaderResourceStream() {
+        val stream = javaClass.classLoader.getResourceAsStream("hardToRead.json")
+        assertNotNull(stream)
+        val string = stream.bufferedReader().use { it.readText() }
+        assertEquals("content length", 38, string.length)
     }
 }
